ebfb94cb5d452963486882f3e684c0ecd70789b2
mstrv <furry@mstrv.pl>
2024-05-08 23:40:41 +0200

    Minor improvements.

--- pages/404.js
+++ pages/404.js
@@ -5,8 +5,7 @@ const Custom404 = () => {
   return (
     <div className="wrapper flex flex-col items-center justify-center min-h-screen">
       <div className="content text-center">
-      <center><h1 className="text-3xl font-bold text-gray-800 mb-4">404 - Nothing here.</h1></center>
-      <center><p className="text-lg text-gray-600 mb-8">The file you are looking for does not exist.</p></center>
+        <img src="https://http.cat/images/404.jpg"/>
         <Link href="/" passHref>
         <center><div className="text-blue-500 hover:underline cursor-pointer">$ cd ~</div></center>
         </Link>
