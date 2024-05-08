12b233c9df7f9bd6d2df4b4920be969877d010a2
mstrv <furry@mstrv.pl>
2024-05-08 15:00:37 +0200

    Minor improvements.

--- pages/FullDescription.js
+++ pages/FullDescription.js
@@ -2,7 +2,7 @@ export default function FullDescription() {
     return (
       <div className="full-description mb-8">
         <h2 className="text-2xl font-bold mb-4">Who am I?</h2>
-        <p className="mb-4">I'm Mero, a self-taught programmer since age 12. I have a passion for development, particularly in the field of backend server development. I enjoy working with Linux and riceing it. As a nerd, I'm constantly learning and messing around with new tech. Feel free to hmu on social media!</p>
+        <p className="mb-4">I'm Mero, a self-taught programmer. I have a passion for development, particularly in the field of backend server development. I enjoy working with Linux and riceing it. As a nerd, I'm constantly learning and messing around with new tech. Feel free to hmu on social media!</p>
         <h2 className="text-2xl font-bold mb-4">Any proof of work?</h2>
         <p className="mb-4">You can find my projects and contributions on <a href="https://github.com/merowo" className="text-blue-500" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
         <h2 className="text-2xl font-bold mb-4">Operating Systems</h2>
@@ -19,4 +19,4 @@ export default function FullDescription() {
         </ul>
       </div>
     );  }
-//meow :3
+
